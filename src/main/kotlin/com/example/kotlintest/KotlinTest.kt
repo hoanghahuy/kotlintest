aa6ec1c82587174ab4fd7be09ee5945ef03c3ec5
Ha Huy Hoang <hoang.hahuy@traveloka.com>
2022-05-31 22:05:57 +0700

    Update main file to test Kotlin with String

--- src/main/kotlin/com/example/kotlintest/KotlinTest.kt
+++ src/main/kotlin/com/example/kotlintest/KotlinTest.kt
@@ -1,10 +1,16 @@
 package com.example.kotlintest
 
 fun main() {
-    print("Hello World")
-    print("Hello World 2")
-    print("Hello World 3")
-    print("Hello World 4")
-    print("Hello World 5")
-    print("Hello World 6")
+    val name = "Hoang Ha Huy"
+//    print("Hello World")
+//    print("Hello World 2")
+//    print("Hello World 3")
+//    print("Hello World 4")
+//    print("Hello World 5")
+//    print("Hello World 6")
+    println("""The Universe are infinite.
+        |And all people are tiny.
+        |Good choice.
+    """.trimMargin())
+    println("Variable \$name are $name")
 }
